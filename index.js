aa70468c4c88817ba8a698ef94e216590d2baa2e
luke <lfairbanks@anamosa.k12.ia.us>
2025-11-17 19:14:40 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+// assign 50 to a variable addition
+let addition = 50;
+// use compound assignment to add 5 to the addition
+addition += 5;
+
+// assign 40 to a variable subtraction
+let subtraction = 40;
+// use compound assignment to subtract 1 from the subtraction
+subtraction -= 1;
+
+// assign 5 to a variable multiplication
+let multiplication = 5;
+// use compound assignment to multiply the multiplication by 7
+multiplication *= 7;
+
+// assign 6 to a variable division
+let division = 6;
+// use compound assignment to divide the division by 2
+division /= 2;
+
+// assign the innerHTML of the HTML element with the id result to the multiplication
+document.getElementById('result').innerHTML = multiplication;
+
